4211ff0df15817174f33cba660a952da3ebcf0d0
fullstackaubrey <aubreywebdev@proton.me>
2023-02-18 16:48:25 +1300

    component name change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Textfield from '@/components/Editbox.tsx';
+import Textfield from '@/components/Textfield.tsx';
 import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
