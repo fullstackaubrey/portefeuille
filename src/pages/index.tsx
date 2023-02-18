4517d2779b73684b7565e719e1a78f5ae265b018
fullstackaubrey <aubreywebdev@proton.me>
2023-02-18 14:16:06 +1300

    styling made responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,37 +7,84 @@ const name = 'Aubrey';
 const job = 'Intermediate Software Engineer';
 const hobby = 'Ashtanga Yoga Practitioner';
 const discovery = 'Web Development';
-const year = '2014';
+const year = '2015';
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-slate-400">
+    <div className="flex h-screen w-screen bg-slate-600">
       <Head>
         <title>Aubrey Dill | Portefeuille</title>
       </Head>
 
-      <nav className="sticky top-0 w-screen h-content cursor-pointer">
-        <ul className="w-full flex flex-row justify-evenly p-3 bg-red-400 text-white shadow-sm shadow-zinc-500">
-          <Link className="self-center" href="/">
-            Home
-          </Link>
-          <li className="self-center">About Me</li>
-          <li className="self-center">Resume</li>
-          <li className="self-center">Projects</li>
-          <div className="w-1/5 p-1 shadow-inner shadow-zinc-500 flex flex-row justify-around bg-slate-100 rounded-sm text-sm text-slate-600 text-center">
-            <li className="p-1 w-1/2 hover:shadow-inner hover:shadow-zinc-400 hover:rounded-sm duration-300 border-r border-slate-300 mr-1">
-              Sign in
-            </li>
-            <li className="p-1 w-1/2 hover:shadow-inner hover:shadow-zinc-400 hover:rounded-sm duration-300 border-l border-slate-300 ml-1">
-              Register
-            </li>
-          </div>
-        </ul>
-      </nav>
-      <div className="flex">
-        <main className="h-2/3 flex flex-row bg-slate-100 ml-8 mb-8 mt-8 shadow-md shadow-zinc-600 cursor-pointer hover:shadow-zinc-700">
-          <section className="mx-6 my-6 w-2/3 bg-slate-200 shadow-md shadow-zinc-400 rounded-sm hover:shadow-zinc-500">
-            <header className="p-4 text-center text-lg text-white tracking-wider font-semibold bg-slate-400">
+      <div className="h-full w-2/3 bg-red-300">
+        <div className="flex justify-center my-4 ml-4 mr-2 h-full bg-red-100">
+          <section className="flex self-center w-full mx-6 h-5/6 bg-slate-400 shadow-sm shadow-zinc-400 cursor-pointer">
+            <div className="w-3/5 h-full hover:shadow-md hover:shadow-zinc-600">
+              <header className="p-3 bg-red-400 text-zinc-100 text-xl text-center font-semibold tracking-wider border-y border-zinc-500 shadow shadow-zinc-500">
+                Portefeuille d'Aubrey
+              </header>
+              <section className="m-8 text-slate-800 text-lg">
+                <p className="p-2">Hi there! My name is {name}.</p>
+                <p className="p-2">
+                  I'm a {job} and {hobby}.
+                </p>
+                <p className="p-2">
+                  Lorem ipsum dolor sit amet. Non Quis praesentium est dicta
+                  quam qui voluptatem veritatis hic dolore fugiat id dignissimos
+                  velit. Est ducimus facilis nam cupiditate nihil qui beatae
+                  asperiores hic repudiandae impedit. Eos rerum temporibus ut
+                  illum galisum aut voluptas beatae eum praesentium aspernatur
+                  et dolorum alias cum voluptatem ipsa et incidunt quam.
+                </p>
+              </section>
+            </div>
+            <div className="flex justify-center w-2/5 bg-red-200 shadow-inner shadow-zinc-400 hover:shadow-zinc-600 rounded-sm">
+              <div className="self-center w-72 h-72 bg-red-500 shadow-md shadow-zinc-400 hover:shadow-zinc-600 rounded-full" />
+            </div>
+          </section>
+        </div>
+      </div>
+      <div className="h-full w-1/3 bg-red-300">
+        <div className="flex justify-center my-4 ml-2 mr-4 h-full bg-red-100">
+          <section className="self-center overflow-y-scroll scroll-smooth w-full mx-6 h-5/6 bg-red-300 shadow-md shadow-zinc-600 hover:shadow-lg hover:shadow-zinc-600 rounded-sm">
+            <div className="m-8 bg-red-200 shadow-inner shadow-zinc-600 rounded">
+              <section className="m-4 text-slate-800 text-sm">
+                <p className="p-2">
+                  Lorem ipsum dolor sit amet. Vel necessitatibus omnis 33 omnis
+                  optio sed veniam excepturi sit dolor architecto sit voluptatem
+                  repudiandae ut voluptatum maiores qui velit exercitationem! Ea
+                  nemo numquam non repellendus mollitia ut ducimus sint qui
+                  quaerat omnis. Vel maiores consectetur vel internos blanditiis
+                  qui reprehenderit sequi qui magnam officia?
+                </p>
+                <p className="p-2">
+                  Qui repudiandae harum qui distinctio amet non tempore nobis.
+                  Rem odio ducimus non natus nostrum id omnis aliquam eos
+                  praesentium veniam sit commodi voluptas sit accusamus porro
+                  aut modi commodi. Est cumque consectetur vel dolorum aperiam
+                  qui adipisci omnis est rerum sunt est quae aliquid nam
+                  mollitia porro ut reprehenderit repellat! Et modi quaerat ut
+                  magnam dolorem quo maxime laudantium.
+                </p>
+                <p className="p-2">
+                  Sed excepturi Quis rem velit omnis et obcaecati esse in
+                  ducimus blanditiis est inventore praesentium. Qui laborum
+                  alias non earum deserunt id consequatur officia aut ipsum esse
+                  et totam deserunt. Ea corporis dolore est minus dolores et
+                  consequatur corporis et ipsa earum ea quas modi! Id voluptate
+                  beatae est suscipit blanditiis ut assumenda explicabo.
+                </p>
+              </section>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+
+    /* <div className="flex ml-8 mr-8 mt-8 mb-8">
+        <main className="flex flex-row  bg-slate-100 shadow-md shadow-zinc-600  hover:shadow-zinc-700 cursor-pointer">
+          <section className="mx-6 my-6 w-2/3 bg-slate-200 shadow-md shadow-zinc-400 hover:shadow-zinc-500 rounded-sm">
+            <header className="p-4  bg-slate-400  text-white text-center text-lg tracking-wider font-semibold">
               Portefeuille d'Aubrey
             </header>
             <div className="m-8 text-md tracking-wider">
@@ -62,14 +109,14 @@ export default function Home() {
             </div>
           </section>
 
-          <section className="w-1/3 flex justify-center shadow-inner shadow-zinc-400 hover:shadow-zinc-500 bg-slate-200">
-            <div className="m-6 w-56 h-56 self-center rounded-full shadow-sm shadow-zinc-600 bg-red-400 hover:scale-105 duration-500" />
+          <section className="w-1/3 flex justify-center  bg-slate-200 shadow-inner shadow-zinc-400 hover:shadow-zinc-500">
+            <div className="m-6 w-56 h-56 self-center  bg-red-400 shadow-sm shadow-zinc-600 hover:scale-105 duration-500 rounded-full" />
           </section>
         </main>
 
-        <div className=" bg-slate-200 w-2/3 h-96 overflow-y-scroll scroll-smooth mx-8 mt-8 shadow-md  shadow-zinc-500 text-zinc-800 rounded-sm hover:shadow-zinc-600 cursor-pointer">
+        {/* <div className="overflow-y-scroll scroll-smooth bg-slate-200 text-zinc-800 shadow-md  shadow-zinc-500 hover:shadow-zinc-600 rounded-sm cursor-pointer">
           <section className="m-6">
-            <div className=" bg-slate-100 p-6 shadow-inner shadow-zinc-500 rounded hover:bg-slate-500 hover:shadow-zinc-600 hover:text-slate-100 duration-600">
+            <div className=" bg-slate-100 p-6 shadow-inner shadow-zinc-500 hover:bg-slate-500 hover:shadow-zinc-600 hover:text-slate-100 duration-600 rounded">
               <p className="p-1">
                 But I must explain to you how all this mistaken idea of
                 denouncing of a pleasure and praising pain was born and I will
@@ -109,55 +156,6 @@ export default function Home() {
                 selection: he rejects pleasures to secure other greater
                 pleasures, or else he endures pains to avoid worse pains.
               </p>
-            </div>
-          </section>
-        </div>
-      </div>
-
-      {/* <header className="bg-green-300">About Me</header>
-        <section className="bg-green-200">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </section>
-
-        <header className="bg-purple-300">Resume</header>
-        <section className="bg-purple-200">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </section>
-
-        <header className="bg-orange-300">Projects</header>
-        <section className="bg-orange-200">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </section>
-
-        <header className="bg-yellow-300">Sign in</header>
-        <section className="bg-yellow-200">Sign in comp</section>
-
-        <header className="bg-red-300">Register</header>
-        <section className="bg-red-200">Register comp</section> */}
-    </div>
+            </div> */
   );
 }
